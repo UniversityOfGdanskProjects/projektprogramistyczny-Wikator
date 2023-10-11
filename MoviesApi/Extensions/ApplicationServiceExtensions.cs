1b0e6a67d8755afe6b227e01b54bddaf11dd5b16
Wiktor Szymulewicz <wiktor@szymulewicz.com>
2023-10-11 22:08:07 +0200

    Initial commit

---
+++ MoviesApi/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Neo4j.Driver;
+
+namespace MoviesApi.Extensions
+{
+	public static class ApplicationServiceExtensions
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+			IConfiguration config)
+		{
+			string server = config.GetConnectionString("Server")
+				?? throw new Exception("Connection string not found");
+
+			string userName = config.GetConnectionString("UserName")
+				?? throw new Exception("Connection string not found");
+
+			string password = config.GetConnectionString("Password")
+				?? throw new Exception("Connection string not found");
+
+			services.AddSingleton(GraphDatabase.Driver(server, AuthTokens.Basic(userName, password)));
+
+			return services;
+		}
+	}
+}
---
+++ MoviesApi/Program.cs
@@ -0,0 +1,21 @@
+using MoviesApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
