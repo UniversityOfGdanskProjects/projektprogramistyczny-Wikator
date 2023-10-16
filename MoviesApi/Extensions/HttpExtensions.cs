6fa589b27e72e2857a7e737c4bc0eff7e0dbed95
Wiktor Szymulewicz <wiktor@szymulewicz.com>
2023-10-16 20:54:36 +0200

    Added pagination helpers

---
+++ MoviesApi/Extensions/HttpExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MoviesApi.Helpers;
+using System.Text.Json;
+
+namespace MoviesApi.Extensions
+{
+	public static class HttpExtensions
+	{
+		public static void AddPaginationHeader(this HttpResponse response, PaginationHeader paginationHeader)
+		{
+			JsonSerializerOptions options = new()
+			{
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+			};
+
+			response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader, options));
+			response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+		}
+	}
+}
