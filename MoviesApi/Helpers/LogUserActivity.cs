af6e7908b882f3b51f818abbbb29732aba8f000e
Wiktor Szymulewicz <wiktor@szymulewicz.com>
2024-01-05 19:04:21 +0100

    Used fire and forget in user activity logging to improve performance

--- MoviesApi/Helpers/LogUserActivity.cs
+++ MoviesApi/Helpers/LogUserActivity.cs
@@ -2,29 +2,38 @@
 using MoviesApi.Services.Contracts;
 using Neo4j.Driver;
 
-namespace MoviesApi.Helpers;
-
-public class LogUserActivity : IAsyncActionFilter
+namespace MoviesApi.Helpers
 {
-    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    public class LogUserActivity : IAsyncActionFilter
     {
-        var resultContext = await next();
-        
-        if (resultContext.HttpContext.User.Identity is not { IsAuthenticated: true })
-            return;
-        
-        var driver = resultContext.HttpContext.RequestServices.GetRequiredService<IDriver>();
-        var claimsProvider = resultContext.HttpContext.RequestServices.GetRequiredService<IUserClaimsProvider>();
-        var userId = claimsProvider.GetUserId(resultContext.HttpContext.User);
-        
-        // language=Cypher
-        const string query = """
-                             MATCH (u:User { Id: $userId })
-                             SET u.LastActive = datetime()
-                             """;
-        
-        await using var session = driver.AsyncSession();
-        await session.ExecuteWriteAsync(tx => tx.RunAsync(query,
-            new { userId = userId.ToString()}));
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+
+            if (!resultContext.HttpContext.User.Identity?.IsAuthenticated ?? false)
+                return;
+
+            var driver = resultContext.HttpContext.RequestServices.GetRequiredService<IDriver>();
+            var claimsProvider = resultContext.HttpContext.RequestServices.GetRequiredService<IUserClaimsProvider>();
+            var userId = claimsProvider.GetUserId(resultContext.HttpContext.User);
+
+
+            _ = LogUserActivityInBackground(driver, userId);
+        }
+
+        private static async Task LogUserActivityInBackground(IDriver driver, Guid userId)
+        {
+            // language=Cypher
+            const string query =  """
+                                  MATCH (u:User { Id: $userId })
+                                  SET u.LastActive = datetime()
+                                  """;
+            
+            await using var session = driver.AsyncSession();
+            await session.ExecuteWriteAsync(async tx =>
+            {
+                await tx.RunAsync(query, new { userId = userId.ToString() });
+            });
+        }
     }
 }
