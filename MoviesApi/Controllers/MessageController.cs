9964787156ddaabc923d89751734f3b72657ae5b
Wikator <wiktor@szymulewicz.com>
2024-01-28 16:39:18 +0100

    Publishing MQTT message on POST message

--- MoviesApi/Controllers/MessageController.cs
+++ MoviesApi/Controllers/MessageController.cs
@@ -1,17 +1,22 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MoviesApi.Controllers.Base;
 using MoviesApi.DTOs.Requests;
+using MoviesApi.DTOs.Responses;
 using MoviesApi.Extensions;
 using MoviesApi.Repository.Contracts;
+using MoviesApi.Services.Contracts;
 using Neo4j.Driver;
 
 namespace MoviesApi.Controllers;
 
 [Route("api/[controller]")]
-public class MessageController(IDriver driver, IMessageRepository messageRepository) : BaseApiController(driver)
+public class MessageController(IDriver driver, IMessageRepository messageRepository,
+    IMqttService mqttService) : BaseApiController(driver)
 {
     private IMessageRepository MessageRepository { get; } = messageRepository;
+    private IMqttService MqttService { get; } = mqttService;
     
     [HttpGet]
     public Task<IActionResult> GetMostRecentMessagesAsync()
@@ -31,7 +36,19 @@ public class MessageController(IDriver driver, IMessageRepository messageReposit
         {
             var userId = User.GetUserId();
             var message = await MessageRepository.CreateMessageAsync(tx, userId, messageDto.Content);
-            return Ok(message);
+            _ = PublishMqttMessageAsync(message!);
+            return Ok(message!);
         });
+    }
+    
+    private async Task PublishMqttMessageAsync(MessageDto messageDto)
+    {
+        JsonSerializerOptions options = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+        
+        var payload = JsonSerializer.Serialize(messageDto, options);
+        await MqttService.SendNotificationAsync("chat/message/validated", payload);
     }
 }
